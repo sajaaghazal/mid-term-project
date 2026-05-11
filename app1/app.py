0fead19fbd609eaa08ddaf825d18475c2d58189f
Sajaa Ghazal <sajaa.ghazale@gmail.com>
2026-05-11 16:09:48 +0200

    final clean code for app1 and dockerfile

--- app1/app.py
+++ app1/app.py
@@ -3,13 +3,17 @@ import redis
 
 app = Flask(__name__)
 
+# Connecting to the 'redis' service defined in docker-compose
 r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # Increment visit count in Redis
     r.incr('visit_count')
-    if request.method == 'POST':
+    
+    if request.method == 'POST': 
         message = request.form['message']
+        # Push the message to a list in Redis
         r.rpush('messages', message)
 
     return '''
@@ -21,4 +25,5 @@ def home():
     '''
 
 if __name__ == '__main__':
+    # host='0.0.0.0' is required for the app to be reachable from outside the container
     app.run(host='0.0.0.0', port=5000)
